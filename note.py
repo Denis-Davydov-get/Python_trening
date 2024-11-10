1339ecb6b43a34c76a78aa5ed3dbba9315c39e4a
Denis-Davydov-get <slim2001.88@mail.ru>
2024-11-10 09:56:08 +0300

    add new work

--- note.py
+++
@@ -1,6 +0,0 @@
-def sum_stydent(stydents:str)-> str:
-    stydents_list = stydents.split(',')
-    return sum(stydents_list)
-
-text = input("Введите фамишию и оценки студента через запятную: ")
-print(sum_stydent(text))
