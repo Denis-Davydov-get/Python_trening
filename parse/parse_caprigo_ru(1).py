fe4aadb289de619687f3aa46168809ae58b0819d
Denis-Davydov-get <slim2001.88@mail.ru>
2025-04-05 09:00:40 +0300

    del dubl main

---
+++ parse/parse_caprigo_ru(1).py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.caprigo.ru/catalog/19f69068-cd49-11e7-99d1-005056c00008?inRow=four&priceMax=330010&priceMin=10278&withStockBalance=1'
+response = requests.get(url).content
+soup = BeautifulSoup(response, 'html.parser')
+# with open('1.txt', 'w', encoding="utf-8") as f:
+#     f.write(soup)
+# mydivs = soup.find_all("div", {"class": "products-item__footer"})
+my_divs = soup.find_all("div")
+print(my_divs)
