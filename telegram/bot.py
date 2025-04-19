43ce51c885d14e68b9789009b9f0e913adefcfd4
Denis Davydov <slim2001.88@mail.ru>
2025-04-19 20:26:21 +0300

    add requirements.txt

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 import time
 
@@ -27,33 +26,3 @@ while counter < MAX_COUNTER:
 
     time.sleep(1)
     counter += 1
-=======
-import requests
-import time
-
-
-API_URL = 'https://api.telegram.org/bot'
-BOT_TOKEN = '5345054662:AAHjmzzNwUXRShqykpUy9rJ1cFnFzLktsTk'
-TEXT = 'Ура! Классный апдейт!'
-MAX_COUNTER = 100
-
-offset = -2
-counter = 0
-chat_id: int
-
-
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-
-    time.sleep(1)
-    counter += 1
->>>>>>> 24dc66684f30afe3ba5cce7e4311dc8c449aabcc
