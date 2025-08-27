ca20e332fd0cd7d573328754cfd7d3cf08ace2bb
Denis-Davydov-get <slim2001.88@mail.ru>
2025-08-27 21:46:15 +0300

    add code for parse_mc

--- parse/pase_mc.py
+++ parse/pase_mc.py
@@ -3,24 +3,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_avg_price_from_mc():
-    """
-    @page - страница для парсинга
-    @list_all_sku - список всех sku
-    @list_done_sku - готовый список товаров
-    """
-    list_done_sku = {}
-    page = requests.get("https://metall-dk.ru/catalog/listovoy-prokat/")
+def get_avg_price_from_mc(page: str) -> dict:
+    dict_sku_done = {}
+    list_done_sku_price = []
+    list_done_sku_name = []
+    page = requests.get(page)
     soup = BeautifulSoup(page.content, 'html.parser')
-    list_all_sku = soup.find_all("div", attrs={"class": "cart_in_part item_img "})
-    # for sku in list_all_sku:
-    #     list_done_sku[sku.find("a").text] = sku.find("em", attrs={"class": "unit_price"})
-    for sku in list_all_sku:
-        return sku.find_all("a").text
+    list_all_sku_name = soup.find_all("div", attrs={"class": 'refstr _nm'})
+    list_all_sku_price = soup.find_all("div", attrs={"class": 'pr'})
+    for i in range(len(list_all_sku_name)):
+        list_done_sku_name.append(list_all_sku_name[i].text)
+    for i in range(len(list_all_sku_price)):
+        list_done_sku_price.append(list_all_sku_price[i].text)
+    for k, v in zip(list_done_sku_name, list_done_sku_price):
+        dict_sku_done[k] = v
+    return dict_sku_done
 
 
-# df = pd.read_excel("products.xlsx", sheet_name="Products")
-# df['price'] = df['name(ru-ru)'].apply(get_avg_price_from_mc)
-# df.to_excel("products_with_prices.xlsx", index=False)
+data_dict = get_avg_price_from_mc("https://mc.ru/metalloprokat/stal_listovaya_g_k/Page200/2")
+df = pd.DataFrame.from_dict(data_dict).fillna(0, inplace=True)
+df.to_csv('output.csv', index=False)
 
-print(get_avg_price_from_mc())
