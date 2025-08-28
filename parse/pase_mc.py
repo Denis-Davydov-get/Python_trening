6be29cef95fa8c7f9a149caf6a21c6cf1281b0b3
Denis-Davydov-get <slim2001.88@mail.ru>
2025-08-28 21:39:02 +0300

    refactoring parse trek

--- parse/pase_mc.py
+++ parse/pase_mc.py
@@ -9,18 +9,22 @@ def get_avg_price_from_mc(page: str) -> dict:
     list_done_sku_name = []
     page = requests.get(page)
     soup = BeautifulSoup(page.content, 'html.parser')
-    list_all_sku_name = soup.find_all("div", attrs={"class": 'refstr _nm'})
-    list_all_sku_price = soup.find_all("div", attrs={"class": 'pr'})
+    list_all_sku_name = soup.find_all("td", attrs={"class": 'name-cell'})
+    list_all_sku_price = soup.find_all("span", attrs={"class": 'price'})
     for i in range(len(list_all_sku_name)):
-        list_done_sku_name.append(list_all_sku_name[i].text)
+        list_done_sku_name.append(list_all_sku_name[i].text.strip())
     for i in range(len(list_all_sku_price)):
-        list_done_sku_price.append(list_all_sku_price[i].text)
+        list_done_sku_price.append(list_all_sku_price[i].text.strip())
     for k, v in zip(list_done_sku_name, list_done_sku_price):
         dict_sku_done[k] = v
     return dict_sku_done
 
 
-data_dict = get_avg_price_from_mc("https://mc.ru/metalloprokat/stal_listovaya_g_k/Page200/2")
-df = pd.DataFrame.from_dict(data_dict).fillna(0, inplace=True)
-df.to_csv('output.csv', index=False)
+def pagination(agent: int) -> dict:
+    for i in range(1, agent):
+        return get_avg_price_from_mc(f"https://glavmetalstal.ru/listovoj-prokat/list-perforirovannyij/?page={i}")
 
+
+df = pd.DataFrame.from_dict(pagination(4), orient='index')
+print(df)
+df.to_excel('data.xlsx', index=True)
